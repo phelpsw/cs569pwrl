96a693027e30280310a93cbf53ff755fa848dbf5
phelpsw <phelpsw@a1adfbe7-6f45-0410-adc5-f5a783de4af1>
2008-04-23 02:56:45 +0000

    Source code modified today has been added.

---
+++ trunk/assn4/src/cs569/camera/FrustumPlane.java
@@ -0,0 +1,40 @@
+package cs569.camera;
+
+import javax.vecmath.Vector3f;
+
+public class FrustumPlane {
+
+	private Vector3f center;
+	private Vector3f normal;
+
+	public FrustumPlane()
+	{
+		
+	}
+	
+	public void setNormalAndPoint(Vector3f normal, Vector3f point)
+	{
+		this.center = point;
+		this.normal = normal;
+	}
+	
+	public Vector3f getCenter()
+	{
+		return center;
+	}
+	
+	public Vector3f getNormal()
+	{
+		return normal;
+	}
+	
+	public float distance(Vector3f pt)
+	{
+		float tmp = ((normal.dot(pt)) / (normal.dot(normal)));
+		Vector3f closestPoint = new Vector3f(normal);
+		closestPoint.scale(tmp);
+		pt.sub(closestPoint);
+		return pt.length();
+	}
+}
+
