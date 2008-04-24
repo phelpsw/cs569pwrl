8df09d85d8c609b59286e6dac7e3837d03336ee9
signal20k <signal20k@a1adfbe7-6f45-0410-adc5-f5a783de4af1>
2008-04-24 22:40:58 +0000

    Fully working, but kinda slow

--- trunk/assn4/src/cs569/object/OctTree.java
+++ trunk/assn4/src/cs569/object/OctTree.java
@@ -9,6 +9,14 @@ import cs569.misc.GLSLErrorException;
 public class OctTree {
 
 	private OctNode root;
+	private float minx = Float.MAX_VALUE;
+	private float miny = Float.MAX_VALUE;
+	private float minz = Float.MAX_VALUE;
+	
+	private float maxx = Float.MIN_VALUE;
+	private float maxy = Float.MIN_VALUE;
+	private float maxz = Float.MIN_VALUE;
+	
 	
 	public OctTree(Vector3f minPoint, float sceneWidth)
 	{
@@ -17,9 +25,35 @@ public class OctTree {
 	
 	public boolean insert(MeshObject o)
 	{
+		Vector3f p = o.boundingBox.getMinPoint();
+		if (minx > p.x)
+		  minx = p.x;
+		if (miny > p.y)
+		 miny = p.y;
+		if (minz > p.z)
+			 minz = p.z;
+		
+		p = o.boundingBox.getMaxPoint();
+		if (maxx < p.x)
+		  maxx = p.x;
+		if (maxy < p.y)
+		 maxy = p.y;
+		if (maxz < p.z)
+			 maxz = p.z;
+		
 		if(root.insert(o) == 0)
+		{
+			System.out.println("won't fit: BB=" + o.boundingBox + ", node :" + root);		
+			
 			return false;
+		}
 		return true;
+	}
+	
+	public void printExtreme()
+	{
+		System.out.println("min: " + minx + ", " + miny + ", " + minz);
+		System.out.println("max: " + maxx + ", " + maxy + ", " + maxz);		
 	}
 	
 	public void renderTree(GL gl, GLU glu, Vector3f eye) throws GLSLErrorException
