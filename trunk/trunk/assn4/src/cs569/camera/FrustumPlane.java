8185f64e82fdb5ee4bba08efed9d3ad6c6f148aa
phelpsw <phelpsw@a1adfbe7-6f45-0410-adc5-f5a783de4af1>
2008-04-24 15:55:48 +0000

    frustum planes appear to be correct now.

--- trunk/trunk/assn4/src/cs569/camera/FrustumPlane.java
+++ trunk/trunk/assn4/src/cs569/camera/FrustumPlane.java
@@ -14,12 +14,24 @@ public class FrustumPlane
 	}
 	
 	//set normal and center of plane
-	public void setNormalAndPoint(Vector3f normal, Vector3f point)
+	public void setNormalAndPoint(Vector3f norm, Vector3f point)
 	{
-		this.normal.set(normal);
-		this.normal.normalize();
+		normal.set(norm);
+		normal.normalize();
 		
-		offset = -(this.normal.dot(point));
+		offset = -(normal.dot(point));		
+	}
+	
+	public void setCoefficients(float a, float b, float c, float d) 
+	{
+		// set the normal vector
+		normal.set(a,b,c);
+		//compute the length of the vector
+		float l = normal.length();
+		// normalize the vector
+		normal.set(a/l,b/l,c/l);
+		// and divide d by th length as well
+		offset = d/l;
 	}
 	
 	public float getOffset()
